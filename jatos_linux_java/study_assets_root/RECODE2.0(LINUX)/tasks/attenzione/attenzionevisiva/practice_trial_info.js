217de9d527440a1fb3671c1f456a665aff57cc89
Bezze95 <bets95@hotmail.it>
2022-08-03 15:42:16 +0200

    commit to master

---
+++ jatos_linux_java/study_assets_root/RECODE2.0(LINUX)/tasks/attenzione/attenzionevisiva/practice_trial_info.js
@@ -0,0 +1,14 @@
+var practice_trial_info = [
+    {
+        number: '2',
+        correctResponse: '4'
+    },
+    {
+        number: '4',
+        correctResponse: '2'
+    },
+    {
+        number: '7',
+        correctResponse: null
+    }
+]; 
